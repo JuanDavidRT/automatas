d2c6d5efd6355cbdf65a0291db2b34a85c089c6a
Luiggi Alvarez <luiggibikes@gmail.com>
2020-05-28 15:17:09 -0500

    Creado menú principal, (faltan ajustes) y los métodos para afd y afd con detalles funcionando

--- src/Automata/Automatas.java
+++ src/Automata/Automatas.java
@@ -3,10 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 package Automata;
+
 import Controller.AFDController;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
@@ -15,24 +16,80 @@ import Controller.AFDController;
 public class Automatas {
 
     public static AFDController afdController;
+
     /**
      * @param args the command line arguments
      */
-    public Automatas(){
-        
+    public Automatas() {
+
     }
-    
+
     public static void main(String[] args) {
         // TODO code application logic here
-        afdController= new AFDController();
-        
-       boolean result= afdController.procesarCadena();
-        if(result){
-            System.out.println("La cadena es aceptada");
+        afdController = new AFDController();
+
+        Scanner sn = new Scanner(System.in);
+        int option, optionIn;
+        boolean exit = false;
+        boolean result=false;
+        while (!exit) {
+
+            System.out.println("Automatas");
+            System.out.println(" ");
+            System.out.println("1.AFD");
+            System.out.println("2.AFN");
+            System.out.println("3.AFN-lambda");
+            System.out.println("4. Salir");
+
+            try {
+
+                option = sn.nextInt();
+
+                switch (option) {
+                    case 1:
+                        System.out.println("Autómata determinista");
+                        System.out.println("Escribe una de las opciones");
+                        System.out.println("1.Procesar Cadena");
+                        System.out.println("2.Procesar Cadena con detalles");
+                        System.out.println("3.Procesar lista de cadenas");
+                        optionIn = sn.nextInt();
+                        switch (optionIn) {
+                            case 1:
+                                result = afdController.procesarCadena();
+                                if (result) {
+                                    System.out.println("La cadena es aceptada");
+                                } else {
+                                    System.out.println("La cadena NO es aceptada");
+                                }
+                                break;
+                                case 2:
+                                result = afdController.procesarCadenaConDetalle();
+                                if (result) {
+                                    System.out.println("La cadena es aceptada");
+                                } else {
+                                    System.out.println("La cadena NO es aceptada");
+                                }
+                                break;
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Autómata no determinista");
+                        break;
+                    case 3:
+                        System.out.println("Autómata lambda no determinista");
+                        break;
+                    case 4:
+                        exit = true;
+                        break;
+                    default:
+                        System.out.println("Solo números entre 1 y 4");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sn.next();
+            }
         }
-        else{
-            System.out.println("La cadena NO es aceptada");
-        }
+
     }
-    
+
 }
--- src/Controller/AFDController.java
+++ src/Controller/AFDController.java
@@ -61,6 +61,23 @@ public class AFDController {
                 
         return result;
     }
+    public boolean procesarCadenaConDetalle(){
+        String cadena= "ababab";
+        String buffer=afd.InitialState;
+        boolean result = false;
+        for(int i=0;i<cadena.length();i++){  //un loop para leer la cadena de entrada
+            char read=cadena.charAt(i);
+            buffer=procesarTransicionConDetalle(afd.Transition, buffer, read);//procesa el caracter que está en la posicion de i del string
+            result=false;           //reinicializo en false para cada iteración
+            for(int j=0;j<afd.AcceptanceState.length;j++){
+                if(buffer.equals(afd.AcceptanceState[j])){
+                    result=true;                                // dice si es aceptado o no
+                }
+            }            
+        }
+                
+        return result;
+    }
     
     public String procesarTransicion(String[][] transicion, String buffer, char read)
     {
@@ -77,6 +94,23 @@ public class AFDController {
             }
         }       
         
+        return bufferResult;
+    }
+    public String procesarTransicionConDetalle(String[][] transicion, String buffer, char read)
+    {
+        String bufferResult=new String();
+        for(int i=0;i<afd.States.length;i++){               //un loop para la cantidad de estados
+            if(buffer.equals(afd.States[i])){               //si el estado actual buffer es igual a un estado en el automata ocntinua
+                
+                for(int j=0; j<afd.Sigma.length; j++){      //loop para el alfabeto
+                    if(read==afd.Sigma[j]){                 //si el caracter es igual a la posicioòn de sigma entra, 
+                        bufferResult =transicion[i][j];    //teniendo en cuenta que en el array de transicion la primera columa son los estados
+                                                            //y la segunda columna los posibles caracteres
+                    }
+                }           
+            }
+        }       
+        System.out.println("En el estado " +buffer+" Lee "+ read + " y pasa al estado " + bufferResult);
         return bufferResult;
     }
 }
